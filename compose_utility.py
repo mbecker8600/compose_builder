f2c16a45e369ce43f5dd877e178a56a15837479d
michaelbecker <michaelbecker022@gmail.com>
2018-10-08 07:41:19 -0400

    renamed files

--- compose_utility.py
+++ compose_utility.py
@@ -5,22 +5,10 @@ import uuid
 from abc import ABC, abstractmethod
 
 
-class ComposeBuilderFactory:
-
-    def getBuilder(self, init_load, primary_key, track_history):
-        if track_history:
-            return ComposeBuilderTypeFourData(init_load, primary_key)
-        else:
-            return ComposeBuilderTypeOneData(init_load, primary_key)
-
-
 class ComposeBuilder(ABC):
 
-    def __init__(self, init_load, primary_key):
+    def __init__(self):
         self.current_table = None
-        self.natural_key = primary_key
-        self.surrogate_key = 'address_sk'
-        self.initialize_compose_layer(init_load=init_load)
 
     @abstractmethod
     def initialize_compose_layer(self):
@@ -31,7 +19,7 @@ class ComposeBuilder(ABC):
         pass
 
     @abstractmethod
-    def persist_updates(self, output_dir):
+    def persist_updates(self, output_dir, file):
         pass
 
     def generate_surrogate_keys(self, n_keys=1):
@@ -56,14 +44,61 @@ class ComposeBuilder(ABC):
         return existing_records, new_records
 
 
-class ComposeBuilderTypeFourData(ComposeBuilder):
+class BasicComposeBuilder(ComposeBuilder):
 
     def __init__(self, init_load, primary_key):
-        self.historical_table = None
-        super().__init__(init_load, primary_key)
+        self.natural_key = primary_key
+        self.surrogate_key = 'address_sk'
+        self.init_load = init_load
+        self.initialize_compose_layer()
 
-    def initialize_compose_layer(self, init_load):
-        self.current_table = init_load.copy()
+    def initialize_compose_layer(self):
+        self.current_table = self.init_load.copy()
+
+    def ingest_cdc(self, cdc):
+        existing_records, new_records = self.split_cdc_records(cdc)
+        self.__ingest_existing_records__(existing_records)
+        self.__ingest_new_records__(new_records)
+
+    def persist_updates(self, output_dir, file):
+        self.current_table.to_csv(output_dir + 'current/' + file)
+
+    def __ingest_existing_records__(self, cdc):
+        self.current_table.ix[cdc.index] = cdc
+
+    def __ingest_new_records__(self, cdc):
+        self.current_table = self.current_table.append(cdc)
+
+
+class ComposeBuilderDecorator(ComposeBuilder):
+
+    def __init__(self, builder):
+        self.builder = builder
+        self.current_table = builder.current_table
+        self.natural_key = builder.natural_key
+        self.surrogate_key = builder.surrogate_key
+        self.init_load = builder.init_load
+
+    def initialize_compose_layer(self):
+        self.builder.initialize_compose_layer()
+
+    def ingest_cdc(self, cdc):
+        self.builder.ingest_cdc(cdc)
+
+    def persist_updates(self, output_dir, file):
+        self.builder.persist_updates(output_dir, file)
+
+
+class TrackHistory(ComposeBuilderDecorator):
+
+    def __init__(self, builder):
+        super().__init__(builder)
+        self.historical_table = None
+        self.initialize_compose_layer()
+
+    def initialize_compose_layer(self):
+        super().initialize_compose_layer()
+
         self.historical_table = init_load.copy()
         self.historical_table['address_sk'] = self.generate_surrogate_keys(
             n_keys=len(init_load))  # create surrogate key
@@ -73,18 +108,17 @@ class ComposeBuilderTypeFourData(ComposeBuilder):
         self.historical_table['effective_end'] = np.nan
 
     def ingest_cdc(self, cdc):
+        super().ingest_cdc(cdc)
+
         existing_records, new_records = self.split_cdc_records(cdc)
         self.__ingest_existing_records__(existing_records)
         self.__ingest_new_records__(new_records)
 
-    def persist_updates(self, output_dir):
-        self.current_table.to_csv(output_dir + 'current/address.csv')
-        self.historical_table.to_csv(output_dir + 'historical/historical_address.csv')
+    def persist_updates(self, output_dir, file):
+        super().persist_updates(output_dir, file)
+        self.historical_table.to_csv(output_dir + 'historical/' + file)
 
     def __ingest_existing_records__(self, cdc):
-        # change current view
-        self.current_table.ix[cdc.index] = cdc
-
         # get only the current records of the historical view
         historical_current_view = self.historical_table[self.historical_table['effective_end'].isnull()]
 
@@ -99,7 +133,6 @@ class ComposeBuilderTypeFourData(ComposeBuilder):
         self.__add_current_historical_records__(cdc)
 
     def __ingest_new_records__(self, cdc):
-        self.current_table = self.current_table.append(cdc)
         self.__add_current_historical_records__(cdc)
 
     def __add_current_historical_records__(self, cdc):
@@ -109,27 +142,9 @@ class ComposeBuilderTypeFourData(ComposeBuilder):
         cdc['effective_end'] = np.nan
         self.historical_table = self.historical_table.append(cdc)
 
-class ComposeBuilderTypeOneData(ComposeBuilder):
 
-    def __init__(self, init_load, primary_key):
-        super().__init__(init_load, primary_key)
-
-    def initialize_compose_layer(self, init_load):
-        self.current_table = init_load.copy()
-
-    def ingest_cdc(self, cdc):
-        existing_records, new_records = self.split_cdc_records(cdc)
-        self.__ingest_existing_records__(existing_records)
-        self.__ingest_new_records__(new_records)
-
-    def persist_updates(self, output_dir):
-        self.current_table.to_csv(output_dir + 'current/transactions.csv')
-
-    def __ingest_existing_records__(self, cdc):
-        self.current_table.ix[cdc.index] = cdc
-
-    def __ingest_new_records__(self, cdc):
-        self.current_table = self.current_table.append(cdc)
+class TrackSchema(ComposeBuilderDecorator):
+    pass
 
 
 if __name__ == '__main__':
@@ -137,22 +152,20 @@ if __name__ == '__main__':
     # test slow changing dimension with history
     # initialize current/historical tables from init file
     init_load = pd.read_csv('raw-landed/scd/init-load/address.csv', index_col=0, parse_dates=['last_updated'])
-    builder_factory = ComposeBuilderFactory()
-    compose_builder = builder_factory.getBuilder(init_load=init_load, primary_key='address_key', track_history=True)
+    compose_builder = TrackHistory(BasicComposeBuilder(init_load=init_load, primary_key='address_key'))
 
     # cdc changes
     cdc = pd.read_csv('raw-landed/scd/cdc/basic-cdc.csv', index_col=0, parse_dates=['last_updated'])
     compose_builder.ingest_cdc(cdc)
-    compose_builder.persist_updates('raw-composed/scd/')
+    compose_builder.persist_updates(output_dir='raw-composed/scd/', file='address.csv')
 
     ## test fact with no history
     # initialize current/historical tables from init file
     init_load = pd.read_csv('raw-landed/fact/init-load/transactions.csv', index_col=0, parse_dates=['transaction_date'])
-    builder_factory = ComposeBuilderFactory()
-    compose_builder = builder_factory.getBuilder(init_load=init_load, primary_key='transaction_key', track_history=False)
+    compose_builder = BasicComposeBuilder(init_load=init_load, primary_key='transaction_key')
 
     # cdc changes
     cdc = pd.read_csv('raw-landed/fact/cdc/basic-cdc.csv', index_col=0, parse_dates=['transaction_date'])
     compose_builder.ingest_cdc(cdc)
-    compose_builder.persist_updates('raw-composed/fact/')
+    compose_builder.persist_updates(output_dir='raw-composed/fact/', file='transactions.csv')
 
